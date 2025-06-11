78b5a6a2e55cd68b7e163ec01f2999fd6a3174f0
Hitesh Navare <navarehitesh65@gmail.com>
2025-06-11 10:01:05 +0530

    done

--- apps/excalidraw/app/page.tsx
+++ apps/excalidraw/app/page.tsx
@@ -4,7 +4,7 @@ export const metadata = {
   title: "WhiteBoard collabarative app",
   description: "Create, brainstorm, and innovate together in real-time",
 }
-export default function page() {
+export default function Page() {
   return (
     <div>
        <App />
